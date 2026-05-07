99a2f53c839070e82c9bd6e6393d375dff504c9f
Nardos-Abebe-Ga <nardosabebe95@gmail.com>
2026-05-07 20:02:56 +0300

    setup auth folders nd add splsh screen

---
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () => context.go('/login'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1A60FF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+              child: Image.asset('assets/logo1.png', height: 80),
+            ),
+            const SizedBox(height: 24),
+            const Text('Uni Track', style: TextStyle(fontSize: 32, color: Colors.white, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
