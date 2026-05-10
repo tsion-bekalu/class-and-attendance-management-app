7cf14afedb47b0c80f1160ca10f659900c538532
Tsion Bekalu <tsion.ugr-9277-16@aau.edu.et>
2026-05-10 20:37:59 +0300

    added attendance dialog screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,5 @@
+import 'package:app/features/student/presentation/screens/attendance_analog_screen.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter/material.dart';
 
 // Instructor Screens
 import 'package:app/features/class_management/presentation/screens/class_details_screen.dart';
@@ -25,52 +25,8 @@ import 'package:app/features/student/presentation/screens/timetable_screen.dart'
 import 'package:app/features/student/presentation/screens/attendace_history_screen.dart';
 import 'package:app/features/student/presentation/screens/class_detail_screen.dart';
 import 'package:app/features/student/presentation/screens/attendance_marked_screen.dart';
-import 'package:app/features/student/presentation/screens/attendance_qr_screen.dart';
 import 'package:app/features/class_management/domain/entities/session_record.dart';
 
-
-class EditClassScreen extends StatelessWidget {
-  final String classId;
-  const EditClassScreen({super.key, required this.classId});
-
-  @override
-  Widget build(BuildContext context) =>
-      Scaffold(body: Center(child: Text('Edit Class $classId')));
-}
-
-class StartSessionScreen extends StatelessWidget {
-  final String classId;
-  const StartSessionScreen({super.key, required this.classId});
-
-  @override
-  Widget build(BuildContext context) =>
-      Scaffold(body: Center(child: Text('Start Session $classId')));
-}
-
-class QRDisplayScreen extends StatelessWidget {
-  final String sessionId;
-  const QRDisplayScreen({super.key, required this.sessionId});
-
-  @override
-  Widget build(BuildContext context) =>
-      Scaffold(body: Center(child: Text('QR $sessionId')));
-}
-
-class EnterJoinCodeScreen extends StatelessWidget {
-  const EnterJoinCodeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) =>
-      const Scaffold(body: Center(child: Text('Join Code')));
-}
-
-class StudentAttendanceScreen extends StatelessWidget {
-  const StudentAttendanceScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) =>
-      const Scaffold(body: Center(child: Text('Student Attendance')));
-}
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/student/home',
@@ -87,8 +43,6 @@ final GoRouter appRouter = GoRouter(
     GoRoute(path: '/instructor/class-details/:classId', name: 'class-details', builder: (_,state){
       final id = state.pathParameters['classId']!; 
       return ClassDetailsScreen(classId: id);} ),
-    GoRoute(path: '/instructor/start-session/:classId', name: 'instructor-start-session', builder: (_, state) => StartSessionScreen(classId: state.pathParameters['classId']!)),
-    GoRoute(path: '/instructor/qr-display/:sessionId', name: 'instructor-qr-display', builder: (_, state) => QRDisplayScreen(sessionId: state.pathParameters['sessionId']!)),
     GoRoute(path: '/instructor/timetable', name: 'instructor-timetable', builder: (_, __) => const TimetableScreen()),
     GoRoute(path: '/instructor/create-announcement',name: 'instructor-create-announcement',builder: (context, state) => const CreateAnnouncementScreen(),),
     GoRoute(path: '/instructor/attendance-record',name: 'attendance-record',builder: (context, state) => const AttendanceRecordScreen(),),
@@ -114,29 +68,11 @@ final GoRouter appRouter = GoRouter(
     // Student
     GoRoute(path: '/student/home', name: 'student-home', builder: (context, state) => const StudentHomeScreen()),
     GoRoute(path: '/student/timetable', name: 'student-timetable', builder: (context, state) => const TimetableScreen(),),
-    GoRoute(path: '/student/join-code', name: 'student-join-code', builder: (_, __) => const EnterJoinCodeScreen()),
-    GoRoute(path: '/student/attendance', name: 'student-attendance', builder: (_, __) => const AttendanceQRScreen()),
+    GoRoute(path: '/student/attendance', name: 'student-attendance', builder: (context, state) => const AttendanceScanScreen()),
     GoRoute(path: '/student/attendance-history', name: 'student-attendance-history', builder: (context, state) => const AttendanceHistoryScreen()),
     GoRoute(path: '/student/notifications', name: 'student-notifications', builder: (_, __) => const NotificationsScreen()),
     GoRoute(path: '/student/class', name: 'student-class', builder: (context, state) => ClassDetailScreen(),),
     GoRoute(path: '/attendance-marked', name: 'attendance-marked', builder: (_, __) => const AttendanceMarkedScreen()),
 
-    GoRoute(
-      path: '/instructor/start-session/:classId',
-      name: 'instructor-start-session',
-      builder: (_, state) => StartSessionScreen(classId: state.pathParameters['classId']!),
-    ),
-
-    GoRoute(
-      path: '/instructor/qr-display/:sessionId',
-      name: 'instructor-qr-display',
-      builder: (_, state) => QRDisplayScreen(sessionId: state.pathParameters['sessionId']!),
-    ),
-
-    GoRoute(
-      path: '/instructor/attendance-record',
-      name: 'attendance-record',
-      builder: (_, _) => const AttendanceRecordScreen(),
-    ),
   ]
 );
